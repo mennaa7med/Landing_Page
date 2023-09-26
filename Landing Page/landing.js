5cf90eb749194bb07170c84e53221d348d877973
menna ahmed <114709638+mennaa7med@users.noreply.github.com>
2023-09-26 23:50:52 +0300

    Add files via upload

---
+++ Landing Page/landing.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+gsap.from('.logo', {opacity: 0, duration: 1, delay: 2, y:10})
+gsap.from('.navbar .nav_item', {opacity: 0, duration: 1, delay: 2.1, y:30, stagger: 0.2})
+
+gsap.from('.title', {opacity: 0, duration: 1, delay: 1.6, y:30})
+gsap.from('.description', {opacity: 0, duration: 1, delay: 1.8, y:30})
+gsap.from('.btn', {opacity: 0, duration: 1, delay: 2.1, y:30})
+gsap.from('.image', {opacity: 0, duration: 1, delay: 2.6, y:30})
+
